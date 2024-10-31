8cf541130cea1ad7d930080fd6c9a3c9c723e0a6
ReyCarlosV <carlostes02@gmail.com>
2024-10-31 14:50:41 -0600

    third Commit

--- examenMAIN.py
+++ examenMAIN.py
@@ -2,14 +2,14 @@ import tkinter as tk
 from examenAPI import ExamenApi
 from examenINTERFACE import ExamenInterfaz
 
-# Clase para ejecutar el programa
-class ExamenMain:
-    def __init__(self):
-        ventana = tk.Tk()
-        api_service = ExamenApi("https://671be4242c842d92c381a59c.mockapi.io/Test")
-        ExamenInterfaz(ventana, api_service)
-        ventana.mainloop()
+# Crear la ventana principal
+ventana = tk.Tk()
 
-# Ejecutar el programa
-if __name__ == "__main__":
-    ExamenMain()
+# Instanciar la API para obtener los datos
+api_service = ExamenApi("https://671be4242c842d92c381a59c.mockapi.io/Test")
+
+# Inicializar la interfaz y pasar la ventana y el servicio API
+app = ExamenInterfaz(ventana, api_service)
+
+# Ejecutar la aplicación
+ventana.mainloop()
